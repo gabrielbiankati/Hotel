ecdca4112fe9e0fe198d59b81737634a7a43fc2d
Gabriel Biankati <gabriel.2023014161@alunocesul.com>
2024-09-05 21:51:47 -0300

    alisson

---
+++ Hotel/Hotel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class Hotel
+    {
+        private int _capacidadeMaxima;
+        private List<Reserva> _reservas = new List<Reserva>();
+
+        public Hotel(int capacidadeMaxima) 
+        {
+            _capacidadeMaxima = capacidadeMaxima;
+        }
+
+        public void CriarReserva(DateTime dataCheckIn, DateTime dataCheckOut, int quartos)
+        {
+            // Validações de entrada
+            if (dataCheckIn >= dataCheckOut)
+            {
+                throw new ArgumentException("Data de checkin deve ser" + "anterior a data de checkout");
+            }
+
+            if (quartos > _capacidadeMaxima)
+            {
+                throw new InvalidOperationException("Excede a capacidade máxima de quartos");
+            }
+
+            // Criar uma nova reserva e adicionar a lista 
+            var reserva = new Reserva(dataCheckIn, dataCheckOut, quartos);
+            _reservas.Add(reserva);
+        }
+
+        public void CancelarReserva(DateTime checkIn, DateTime checkOut)
+        {
+            // Remove todas as reservas que coincidem com as datas
+            _reservas.RemoveAll(r => r.DataCheckIn == checkIn && r.DataCheckOut == checkOut);
+        }
+
+        public List<Reserva> ObterReservas()
+        {
+            return _reservas;
+        }
+
+        public decimal CalcularValorTotalReserva(DateTime checkIn, DateTime checkOut, int quartos, decimal precoPorQuarto)
+        {
+            int noites = (checkOut - checkIn).Days;
+            return noites * quartos * precoPorQuarto;
+        }
+    }
+}
---
+++ Hotel/Reserva.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class Reserva
+    {
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+        public int Quartos {  get; set; }
+
+        public Reserva(DateTime dataCheckin, DateTime dataCheckOut, int quartos)
+        {
+            DataCheckIn = dataCheckin;
+            DataCheckOut = dataCheckOut;
+            Quartos = quartos;
+        }
+    }
+}
